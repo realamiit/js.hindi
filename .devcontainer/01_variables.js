749bd87d85388a200a7550c82339074fe67483b9
Amit Gupta <amitgupta4263@gmail.com>
2025-11-12 18:55:31 +0000

    01variables

---
+++ .devcontainer/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 143211
+let accountEmail = "amit@gmail.com"
+var accountPassword = "124566"
+accountCity = "Varanshi"
+let accountState;
+
+
+//accountId = 2 // This will throw an error because accountId is a constant
+accountEmail = "rahul@gmail.com"
+accountPassword = "987654"
+accountCity = "Delhi"
+
+console.log(accountId);
+console.table({accountId, accountEmail,accountPassword,accountCity,accountState});
+
+
+//node /workspaces/js.hindi/.devcontainer/01_variables.js
