201e2d51715ecc2eb01a57ac6717ea279d756865
Amit Gupta <amitgupta4263@gmail.com>
2025-11-13 18:03:37 +0000

    operations

--- .devcontainer/03_conversionOperation.js
+++ .devcontainer/03_conversionOperation.js
@@ -23,6 +23,31 @@ let LoggedIn = 1;
 let booleanIsLoggedIn = Boolean(LoggedIn);
 console.log(typeof LoggedIn); // "boolean"
 
+//******************************Operations**********************************
 
 
-//run code: node .devcontainer/03_conversionOperation.js
+let value = "100";
+let negativeValue = -value;
+console.log(negativeValue); // -100
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(5%2);
+
+let str1 = "Hello ";
+let str2 = "World";
+let str3 = str1 + str2; // "Hello World"
+
+console.log("1"+2); // "12"
+console.log("1"+2 + 2); //122
+console.log(1+"2"); // "12"
+console.log(1+2 + "2"); // "32"32
+console.log(1+"2"+ 2);
+
+
+//run code: 
+ //         node .devcontainer/03_conversionOperation.js
---
+++ .devcontainer/03_conversionOperator.js
@@ -0,0 +1,3 @@
+// Wrapper to match the filename the user tried to run.
+// It forwards execution to the existing file `03_conversionOperation.js`.
+require('./03_conversionOperation.js');
